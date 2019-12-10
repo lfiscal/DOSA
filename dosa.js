c48a69230716856f0ce196fe7a5909e20d944542
Laura <lfiscal@gmail.com>
2019-12-10 08:01:02 -0600

    Create dosa.js

---
+++ dosa.js
@@ -0,0 +1,32 @@
+if (counter) throw counter.init(), "resetting";
+var counter = {
+        docObj: $("<span contenteditable='true' tabindex='-1' style='padding: 6px 8px;border-radius:5px;border:1px solid lightgray; margin: 0px 10px;'>0</span"),
+        projectName: $("#root > div > div.AnnotationPresentation__container--27Z9W > div.AnnotationPresentation__masthead-container--28k_Q > div > div.MastHead__navigation--3R0fi > div:nth-child(2) > p:nth-child(2)").text(),
+        skip: !0,
+        id: "",
+        init: function() {
+            this.docObj.insertBefore("#root > div > div.AnnotationPresentation__container--27Z9W > div.AnnotationPresentation__masthead-container--28k_Q > div > div.MastHead__navigation--3R0fi"), localStorage.getItem(this.projectName) ? this.docObj.text(localStorage.getItem(this.projectName)) : localStorage.setItem(this.projectName, this.docObj.text()), this.onload()
+        },
+        incr: function() {
+            this.docObj.text(String(parseInt(this.docObj.text()) + 1)), localStorage.setItem(this.projectName, this.docObj.text())
+        },
+        onload: function() {
+            this.incr()
+        }
+    },
+    s_ajaxListener = {
+        tempOpen: XMLHttpRequest.prototype.open,
+        tempSend: XMLHttpRequest.prototype.send,
+        callback: function() {
+          this.url.includes("done") && counter.onload()
+        }
+    };
+XMLHttpRequest.prototype.open = function(e, t) {
+    if (!e) e = "";
+    if (!t) t = "";
+    s_ajaxListener.tempOpen.apply(this, arguments), s_ajaxListener.method = e, s_ajaxListener.url = t, "get" == e.toLowerCase() && (s_ajaxListener.data = t.split("?"), s_ajaxListener.data = s_ajaxListener.data[1])
+}, XMLHttpRequest.prototype.send = function(e, t) {
+    if (!e) e = "";
+    if (!t) t = "";
+    s_ajaxListener.tempSend.apply(this, arguments), "post" == s_ajaxListener.method.toLowerCase() && (s_ajaxListener.data = e), s_ajaxListener.callback()
+}, hideParseSetting = !1, counter.init();
