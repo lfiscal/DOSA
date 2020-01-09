e02be01cfe9b28c61bdc9c8f5d3a849e7ebc4155
Laura <lfiscal@gmail.com>
2020-01-09 08:11:25 -0600

    Update dosa.js

--- dosa.js
+++ dosa.js
@@ -7,6 +7,13 @@ marginLeft:0},function(){return a.getBoundingClientRect().left}):0))+"px":void 0
 padding:"inner"+a,content:b,"":"outer"+a},function(c,d){n.fn[d]=function(d,e){var f=arguments.length&&(c||"boolean"!=typeof d),g=c||(d===!0||e===!0?"margin":"border");return Y(this,function(b,c,d){var e;return n.isWindow(b)?b.document.documentElement["client"+a]:9===b.nodeType?(e=b.documentElement,Math.max(b.body["scroll"+a],e["scroll"+a],b.body["offset"+a],e["offset"+a],e["client"+a])):void 0===d?n.css(b,c,g):n.style(b,c,d,g)},b,f?d:void 0,f,null)}})}),n.fn.extend({bind:function(a,b,c){return this.on(a,null,b,c)},unbind:function(a,b){return this.off(a,null,b)},delegate:function(a,b,c,d){return this.on(b,a,c,d)},undelegate:function(a,b,c){return 1===arguments.length?this.off(a,"**"):this.off(b,a||"**",c)}}),n.fn.size=function(){return this.length},n.fn.andSelf=n.fn.addBack,"function"==typeof define&&define.amd&&define("jquery",[],function(){return n});var nc=a.jQuery,oc=a.$;return n.noConflict=function(b){return a.$===n&&(a.$=oc),b&&a.jQuery===n&&(a.jQuery=nc),n},b||(a.jQuery=a.$=n),n});
 
 //END JQUERY
+
+var createCounter = function(){
+	currentNum = localStorage.getItem('DOSA');
+	$("<span id='current'></span").insertAfter("div.MastHead__navigation--3R0fi")
+$('#current').text(currentNum);
+}
+
 if (counter) throw counter.init(), "resetting";
 var counter = {
         docObj: $("<span contenteditable='true'>0</span"),
@@ -28,6 +35,7 @@ var counter = {
         tempSend: XMLHttpRequest.prototype.send,
         callback: function() {
           this.url.includes("done") && counter.onload()
+          createCounter();      
         }
     };
 XMLHttpRequest.prototype.open = function(e, t) {
@@ -38,4 +46,5 @@ XMLHttpRequest.prototype.open = function(e, t) {
     if (!e) e = "";
     if (!t) t = "";
     s_ajaxListener.tempSend.apply(this, arguments), "post" == s_ajaxListener.method.toLowerCase() && (s_ajaxListener.data = e), s_ajaxListener.callback()
-}, hideParseSetting = !1, counter.init()
+}, hideParseSetting = !1, counter.init();
+createCounter();
